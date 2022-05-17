8b82f340db2a413380bdb1b2f4dd332e8f69b8f5
Mikhail Frolov <mf-1992@yandex.ru>
2022-05-17 14:42:50 +0300

    ДЗ 1.2

---
+++ JavaCoreHomework1.2.1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int x : intList) {
+            if ((x > 0) && (x % 2 == 0))
+                result.add(x);
+        }
+        result.sort(Comparator.naturalOrder());
+        for (int i: result)
+        System.out.println(i);
+    }
+}
